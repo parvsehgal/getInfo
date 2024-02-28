f07019d10d2741970d1a1c97f1553a64a4364a94
parvsehgal <parvsehgal2004@gmail.com>
2024-02-28 22:16:33 +0530

    initialized the project

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+
+function App() {
+  return (
+    <div >
+      <h1>this is the Landing page of the website </h1>
+      <input placeholder="enter url for a youtube website" />
+    </div>
+  );
+}
+
+export default App;
